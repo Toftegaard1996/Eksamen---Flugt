960c5ff74073240edc826a3e7e8ed9a2be786a56
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-05-23 13:51:28 +0200

    Flere lag

--- javascript/flugt.js
+++ javascript/flugt.js
@@ -11,7 +11,7 @@ function check(){
     let question1 = document.quiz.question1.value;
     let question2 = document.quiz.question2.value;
     let question3 = document.quiz.question3.value;
-    let question4 = document.question4.value;
+    let question4 = document.quiz.question4.value;
     let question5 = document.quiz.question5.value;
     let question6 = document.quiz.question6.value;
     let correct = 0;
