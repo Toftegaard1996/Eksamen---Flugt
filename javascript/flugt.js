a59edce5391536c4e7c22d8d8fac4e90d2d31e07
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-05-23 10:56:59 +0200

    mere quiz

--- javascript/flugt.js
+++ javascript/flugt.js
@@ -3,6 +3,19 @@ function tilbagequiz() {
     window.location.href = 'login.html';
 }
 
+document.getElementsByName("spørgsmål1").onsubmit=function() {
+       let = parseInt(document.querySelector('input[name = "svarmulighed"]:checked').value);
+	   
+	   
+	   result = variable + sub + con + ifstate;
+	   
+	document.getElementById("grade").innerHTML = result;
+	   
+
+
+return false; // required to not refresh the page; just leave this here
+} //this ends the submit function
+
 /* feedback*/
 
 function tilbage() {
